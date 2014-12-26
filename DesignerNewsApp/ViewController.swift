d97d44588c9ffa0aa1642cce24120c4d711372e1
Meng To <shadownessguy@gmail.com>
2014-12-26 22:20:34 +0800

    Initial Commit

---
+++ DesignerNewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DesignerNewsApp
+//
+//  Created by Meng To on 2014-12-26.
+//  Copyright (c) 2014 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
