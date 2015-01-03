e2983d09e0b77f44c3fcb9c87b4d7d1969814736
Meng To <shadownessguy@gmail.com>
2015-01-03 20:32:48 +0800

    UI for previewing animations

--- DesignerNewsApp/SpringViewController.swift
+++ DesignerNewsApp/SpringViewController.swift
@@ -8,34 +8,102 @@
 
 import UIKit
 
-class SpringViewController: UIViewController {
+class SpringViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var delayLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    @IBOutlet weak var delaySlider: UISlider!
+    @IBOutlet weak var durationSlider: UISlider!
+    @IBOutlet weak var forceSlider: UISlider!
     @IBOutlet weak var ballView: SpringView!
+    @IBOutlet weak var animationPicker: UIPickerView!
+    var selectedRow: Int = 0
+    var selectedForce: CGFloat = 1
+    var selectedDuration: CGFloat = 0.7
+    var selectedDelay: CGFloat = 0
     
-    @IBAction func shakeButtonPressed(sender: AnyObject) {
-        ballView.force = 2
-        ballView.delay = 0.1
-        ballView.animation = "shake"
+    @IBAction func forceSliderChanged(sender: AnyObject) {
+        selectedForce = sender.valueForKey("value") as CGFloat
+        animateView()
+        forceLabel.text = "Force: \(ceil(selectedForce))"
+    }
+    @IBAction func durationSliderChanged(sender: AnyObject) {
+        selectedDuration = sender.valueForKey("value") as CGFloat
+        animateView()
+        durationLabel.text = "Duration: \(ceil(selectedDuration))"
+    }
+    @IBAction func delaySliderChanged(sender: AnyObject) {
+        selectedDelay = sender.valueForKey("value") as CGFloat
+        animateView()
+        delayLabel.text = "Delay: \(ceil(selectedDelay))"
+    }
+    
+    func animateView() {
+        ballView.animation = data[selectedRow]
+        ballView.duration = selectedDuration
+        ballView.delay = selectedDelay
+        ballView.force = selectedForce
         ballView.animate()
     }
-    @IBAction func popButtonPressed(sender: AnyObject) {
-        ballView.force = 3
-        ballView.animation = "pop"
-        ballView.animate()
-    }
-    @IBAction func morphButtonPressed(sender: AnyObject) {
-        let animation = CAKeyframeAnimation()
-        animation.keyPath = "transform.scale"
-        animation.values = [0, 30, -30, 30, 0]
-        animation.keyTimes = [0, 0.2, 0.4, 0.6, 0.8, 1]
-        animation.duration = 1
-        animation.additive = true
-        animation.repeatCount = 1
-        ballView.layer.addAnimation(animation, forKey: "shake")
-    }
+    
+    var data =
+    [
+        "shake",
+        "pop",
+        "morph",
+        "squeeze",
+        "flash",
+        "wobble",
+        "swing",
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "fadeIn",
+        "fadeOut",
+        "fadeInLeft",
+        "fadeInright",
+        "fadeInDown",
+        "fadeInUp",
+        "zoomIn",
+        "zoomOut",
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        animationPicker.delegate = self
+        animationPicker.dataSource = self
+        animationPicker.showsSelectionIndicator = true
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return data[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedRow = row
+        animateView()
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
