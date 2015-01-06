b2812d7c3499d89371c892861471221c8a175af8
Meng To <shadownessguy@gmail.com>
2015-01-07 00:56:17 +0800

    center textfield solution

--- DesignerNewsApp/LoginViewController.swift
+++ DesignerNewsApp/LoginViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var dialogView: SpringView!
     @IBOutlet weak var signupButton: SpringButton!
+    @IBOutlet weak var emailTextField: DesignableTextField!
+    @IBOutlet weak var passwordTextField: DesignableTextField!
+    var originalCenter: CGPoint!
     
     @IBAction func signupButtonPressed(sender: AnyObject) {
         dialogView.resetAll()
@@ -19,13 +22,34 @@ class LoginViewController: UIViewController {
         dialogView.animate()
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(true)
-        
-        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
+    func textFieldDidBeginEditing(textField: UITextField) {
+        spring(0.7) {
+            self.view.center.y = self.originalCenter.y - textField.center.y
+        }
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        spring(0.7) {
+            self.view.center = self.originalCenter
+        }
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        originalCenter = view.center
+        
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent, animated: true)
     }
 }
