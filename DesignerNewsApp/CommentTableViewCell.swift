f8ab4b16a19ca36a5e8b82514e361c36531c8add
André Schneider <hello@andreschneider.me>
2015-01-22 21:52:52 +0100

    Add Comment Table View Cell.

---
+++ DesignerNewsApp/CommentTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CommentTableViewCell.swift
+//  DesignerNewsApp
+//
+//  Created by André Schneider on 22.01.15.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+}
