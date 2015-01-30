1d369288acf6ff8fbe29e685ff238d20652ec489
André Schneider <hello@andreschneider.me>
2015-01-30 15:49:08 +0100

    Rename delegate method.

--- DesignerNewsApp/MenuViewController.swift
+++ DesignerNewsApp/MenuViewController.swift
@@ -13,7 +13,7 @@ protocol MenuViewControllerDelegate : class {
     func menuViewControllerDidSelectTopStories(controller:MenuViewController)
     func menuViewControllerDidSelectRecent(controller:MenuViewController)
     func menuViewControllerDidSelectLogout(controller:MenuViewController)
-    func menuViewControllerDidLogin(controller:MenuViewController)
+    func menuViewControllerDidSelectLogin(controller:MenuViewController)
 }
 
 class MenuViewController: UIViewController, LoginViewControllerDelegate {
@@ -79,14 +79,11 @@ class MenuViewController: UIViewController, LoginViewControllerDelegate {
     
     // MARK: LoginViewControllerDelegate
     func loginViewControllerDidLogin(controller: LoginViewController) {
-        loginCompleted()
+        dismissViewControllerAnimated(true, completion: nil)
+        delegate?.menuViewControllerDidSelectLogin(self)
     }
 
     // MARK: Misc
-    func loginCompleted() {
-        dismissViewControllerAnimated(true, completion: nil)
-        delegate?.menuViewControllerDidLogin(self)
-    }
 
     func animateView() {
         dialogView.animation = "pop"
