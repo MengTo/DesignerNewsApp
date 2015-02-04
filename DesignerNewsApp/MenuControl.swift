c52d67024fa6f0e1e5f0db1ddfd76ca3089d5aaa
André Schneider <hello@andreschneider.me>
2015-02-04 22:30:03 +0100

    Add Menu Control.

---
+++ DesignerNewsApp/MenuControl.swift
@@ -0,0 +1,21 @@
+//
+//  MenuControl.swift
+//  DesignerNewsApp
+//
+//  Created by André Schneider on 04.02.15.
+//  Copyright (c) 2015 Meng To. All rights reserved.
+//
+
+import UIKit
+
+class MenuControl: UIControl {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
