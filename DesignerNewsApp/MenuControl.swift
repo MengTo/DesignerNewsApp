3934c4605ffd30d0757031d44476e91491817074
André Schneider <hello@andreschneider.me>
2015-02-04 22:55:23 +0100

    Add lines.

--- DesignerNewsApp/MenuControl.swift
+++ DesignerNewsApp/MenuControl.swift
@@ -10,4 +10,45 @@ import UIKit
 
 @IBDesignable class MenuControl: UIControl {
 
+    @IBInspectable var color: UIColor = UIColor.whiteColor() {
+        didSet {
+            topView.backgroundColor = color
+            centerView.backgroundColor = color
+            bottomView.backgroundColor = color
+        }
+    }
+
+    private let topView = UIView()
+    private let centerView = UIView()
+    private let bottomView = UIView()
+
+    override func layoutSubviews() {
+        if subviews.isEmpty {
+            setUp()
+        }
+
+    }
+
+    private func setUp() {
+        backgroundColor = UIColor.clearColor()
+        
+        let height = CGFloat(2)
+        let width = bounds.maxX
+
+        topView.frame = CGRectMake(0, bounds.minY, width, height)
+        topView.userInteractionEnabled = false
+        topView.backgroundColor = color
+
+        centerView.frame = CGRectMake(0, bounds.midY - round(height / 2), width, height)
+        centerView.userInteractionEnabled = false
+        centerView.backgroundColor = color
+
+        bottomView.frame = CGRectMake(0, bounds.maxY - height, width, height)
+        bottomView.userInteractionEnabled = false
+        bottomView.backgroundColor = color;
+
+        addSubview(topView)
+        addSubview(centerView)
+        addSubview(bottomView)
+    }
 }
