d9f8789f5dc5c489b8c9276a994a573bf8f804de
André Schneider <hello@andreschneider.me>
2015-07-06 13:17:52 +0200

    Chnage base URL.

--- DesignerNewsApp/DesignerNewsService.swift
+++ DesignerNewsApp/DesignerNewsService.swift
@@ -12,7 +12,7 @@ struct DesignerNewsService {
 
     // Designer News API Doc: http://developers.news.layervault.com
 
-    private static let baseURL = "https://api-news.layervault.com"
+    private static let baseURL = "https://www.designernews.co"
     private static let clientID = "750ab22aac78be1c6d4bbe584f0e3477064f646720f327c5464bc127100a1a6d"
     private static let clientSecret = "53e3822c49287190768e009a8f8e55d09041c5bf26d0ef982693f215c72d87da"
 
